07638630e095b62d4fc15a9f541ca102294551f1
mchalepage <me@mchale.page>
2020-04-10 14:37:28 -0600

    part 1, step 1 complete

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,14 +5,62 @@ class Form extends Component {
     constructor(){
         super()
         this.state = {
-            
-        }
+            imgurl: '',
+            name: '',
+            price: 0
+        }   
+    }
+
+    handleImageURL = (e) => {
+        this.setState({
+            imgurl: e.target.value
+        })
+    }
+
+    handleName = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handlePrice = (e) => {
+        this.setState({
+            price: e.target.value
+        })
+    }
+
+
+    handleResetInputValues = () => {
+        this.setState({
+            imgurl: '',
+            name: '',
+            price: 0
+        })
     }
 
     render(){
         return( 
             <div className='form'>
-                Form
+                <input 
+                    className="image-url-input" 
+                    value={this.state.imgurl}
+                    onChange={(e) => this.handleImageURL(e)}
+                />
+                <input 
+                    className="product-name-input" 
+                    value={this.state.name}
+                    onChange={(e) => this.handleName(e)}
+                />
+                <input 
+                    className="price-input"
+                    value={this.state.price}
+                    onChange={(e) => this.handlePrice(e)}
+                />
+                <button 
+                    className="cancel-button"
+                    onClick={() => this.handleResetInputValues()}    
+                >Cancel</button>
+                <button className="add-to-inventory-button">Add to Inventory</button>
             </div>
         )
     }
